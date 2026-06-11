109aa923f76bd45b17312f27f7815e8e1f3ec5aa
Azhar Haikal Anwar <azharlolypop@gmail.com>
2026-06-12 00:57:30 +0700

    disabled default class test

--- src/test/java/com/vyra/be_virtual_your_assets/VirtualYourAssetApplicationTests.java
+++ src/test/java/com/vyra/be_virtual_your_assets/VirtualYourAssetApplicationTests.java
@@ -1,10 +1,12 @@
 package com.vyra.be_virtual_your_assets;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@Disabled
 @SpringBootTest
 class VirtualYourAssetApplicationTests {
 
